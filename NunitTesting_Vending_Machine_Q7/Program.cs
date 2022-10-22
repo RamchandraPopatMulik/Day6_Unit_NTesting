2882286072e930faef08336d1437146ffa9cf182
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-22 05:33:03 -0400

    added file

--- NunitTesting_Vending_Machine_Q7/Program.cs
+++ NunitTesting_Vending_Machine_Q7/Program.cs
@@ -1,36 +1,58 @@
 ﻿namespace NunitTesting_Vending_Machine_Q7
 {
-    internal class MonthPayment
+    internal class DayOfWeek
     {
-        public static int Principal, Year;
-        public static double Interest;
+        public static int Date, Month, Year;
 
-        public MonthPayment(int principal, int year, double interest)
+        public DayOfWeek(int date, int month, int year)
         {
-            Principal = principal;
+            Date = date;
+            Month = month;
             Year = year;
-            Interest = interest;
-
         }
-        public static void calculatePaymnet()
+        public static void findDayOfWeek()
         {
-            Console.WriteLine("Please Enter Your Principal : ");
-            int principal = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Enter Your Year : ");
+            Console.Write("Enter Date : ");
+            int date = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Month : ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Year : ");
             int year = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Enter Your Interest : ");
-            double interest = Convert.ToDouble(Console.ReadLine());
 
-            int Month = 12 * year;
-            double Rate = interest / (12 * 100);
-            double Payment = (principal * Rate) / (1 - Math.Pow((1 + Rate), -Month));
-            Console.WriteLine("Monthly EMI is : " + Payment);
+            int x = 0, y0 = 0, m0 = 0, d0;
+            y0 = year - (14 - month) / 12;
+            x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            m0 = month + 12 * ((14 - month) / 12) - 2;
+            d0 = (date + x + 31 * m0 / 12) % 7;
+
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Thuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
         }
-
-
         static void Main(string[] args)
         {
-            MonthPayment.calculatePaymnet();
+            DayOfWeek.findDayOfWeek();
         }
     }
 }
